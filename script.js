03d73c04b7f6e144dae2521f9959924c130adf17
Jon Lunsford <jon.lunsford@outlook.com>
2021-03-12 23:27:37 -0600

    layout of HTML form controls

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let range = document.getElementById('slider');
+let output = document.getElementById('rateholder');
+output.innerHTML = range.value;
+
+range.oninput = function () {
+    output.innerHTML = this.value;
+}
+        
+
 function compute()
 {
     p = document.getElementById("principal").value;
     
 }
-        
